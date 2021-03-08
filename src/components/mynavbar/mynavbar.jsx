aeaa309ae264cdb25b051c8d34a4845cf93fed8c
Vimal M <vimalnavee2110@gmail.com>
2021-03-08 09:10:53 +0530

    Bug fix 6

--- src/components/mynavbar/mynavbar.jsx
+++ src/components/mynavbar/mynavbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from 'react-bootstrap/Nav';
 import Logo from "../../assects/img/Nav/vimal.png"
+import Link from "react-scroll";
 import "./mynavbar.css";
 
 
@@ -15,7 +16,7 @@ const MyNavbar=()=>{
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link href="#home" className="nav-active">Home</Nav.Link>
+                    <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#about">About</Nav.Link>
                     <Nav.Link href="#skills">Skills</Nav.Link>
                     <Nav.Link href="#projects">Projects</Nav.Link>
